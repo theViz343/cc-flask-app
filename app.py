051ecdf4b529a642d5c28fa3902a0d9967127051
vishwesh <vishwesh103@gmail.com>
2024-02-14 16:44:02 -0700

    Add load_data fn and endpoint

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ import csv
 
 app = Flask(__name__)
 
+lookup={}
+
+@app.route('/load/"', methods=['GET'])
+def load_data():
+    with open('lookup.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            lookup[row[0]] = row[1]
+    return lookup
+
 @app.route("/", methods=['POST'])
 def process_file():
     f = request.files['inputFile']
     f_name = f.filename.split('.')[0]
     result = "File not found."
-    with open('lookup.csv', mode='r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            if row[0] == f_name:
-                result = f"{row[0]}:{row[1]}"
+    if lookup:
+        result = lookup[f_name]
+    else:
+        load_data()
+        result = lookup[f_name]
     return result
