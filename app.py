38ccf946ab4f88c1856315435f502f08cc5ad608
vishwesh <vishwesh103@gmail.com>
2024-02-16 20:20:52 -0700

    Revert to simple lookup.

--- app.py
+++ app.py
@@ -3,24 +3,15 @@ import csv
 
 app = Flask(__name__)
 
-lookup={}
-
-@app.route('/load/"', methods=['GET'])
-def load_data():
-    with open('lookup.csv', mode='r') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            lookup[row[0]] = row[1]
-    return lookup
-
 @app.route("/", methods=['POST'])
 def process_file():
     f = request.files['inputFile']
     f_name = f.filename.split('.')[0]
     result = "File not found."
-    if lookup:
-        result = f"{f_name}:{lookup[f_name]}"
-    else:
-        load_data()
-        result = f"{f_name}:{lookup[f_name]}"
+
+    with open('lookup.csv', mode='r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == f_name:
+                result = f"{f_name}:{row[1]}"
     return result
